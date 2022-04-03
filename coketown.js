907f4ed8afa6753d1ae1b73f354e7b2def76a875
ayh9601 <ays9499@naver.com>
2022-04-03 22:20:44 +0900

    pass2

--- coketown.js
+++ coketown.js
@@ -1,2 +1,31 @@
 //1번
 console.log("Hello World!");
+
+//2번
+console.log("강한친구 대한육군");
+console.log("강한친구 대한육군");
+
+//3번
+console.log(`\\    /\\`);
+console.log(` )  ( ')`);
+console.log(`(  /  )`);
+console.log(` \\(__)|`);
+
+console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`)
+
+//4번
+console.log(`|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"\`    |
+||_/=\\\\__|`)
+
+//5번
+const fs = require('fs')
+const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+const [a,b] = inputData
+
+console.log(a+b)

942e9502f36c79d2bb13977e733ff5b2a635bf79
ayh9601 <ays9499@naver.com>
2022-04-03 22:43:18 +0900

    pass3

--- coketown.js
+++ coketown.js
@@ -24,8 +24,41 @@ console.log(`|\\_/|
 ||_/=\\\\__|`)
 
 //5번
-const fs = require('fs')
-const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
-const [a,b] = inputData
+// const fs = require('fs')
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+// const [a,b] = inputData
+// 위의 3줄은 JS에서 값을 입력받는 부분이며, 아래의 문제들에서도 공통적으로 사용된다.
 
-console.log(a+b)
+// console.log(a+b)
+
+//6번
+// const fs = require('fs')
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+// const [a,b] = inputData
+
+// console.log(a-b)
+
+//7번
+// const fs = require('fs')
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+// const [a,b] = inputData
+
+// console.log(a*b)
+
+//8번
+// const fs = require('fs')
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+// const [a,b] = inputData
+
+// console.log(a/b)
+
+//9번
+// const fs = require('fs')
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
+// const [a,b] = inputData
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(Math.floor(a/b))
+// console.log(a%b)

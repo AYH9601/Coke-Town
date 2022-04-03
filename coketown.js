3511d7d1243826a686ac03933512db3675607640
ayh9601 <ays9499@naver.com>
2022-04-03 23:21:39 +0900

    pass4

--- coketown.js
+++ coketown.js
@@ -27,7 +27,7 @@ console.log(`|\\_/|
 // const fs = require('fs')
 // const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value)
 // const [a,b] = inputData
-// 위의 3줄은 JS에서 값을 입력받는 부분이며, 아래의 문제들에서도 공통적으로 사용된다.
+// 위의 3줄은 JS에서 숫자값을 입력받는 부분이며, 아래의 문제들에서도 공통적으로 사용된다.
 
 // console.log(a+b)
 
@@ -62,3 +62,43 @@ console.log(`|\\_/|
 // console.log(a*b)
 // console.log(Math.floor(a/b))
 // console.log(a%b)
+
+//10번
+// const fs = require('fs'); 
+// const input = fs.readFileSync("/dev/stdin").toString().trim(); 
+// 위의 2줄은 JS에서 문자값을 입력받는 부분이다.
+// console.log(`${input}??!`);
+
+//11번
+// const fs = require('fs');
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value);
+// const [a] = inputData;
+// console.log(a-543);
+
+//12번
+// const fs = require('fs');
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value);
+// const [a,b,c] = inputData;
+
+// console.log((a+b)%c);
+// console.log(((a%c) + (b%c))%c);
+// console.log((a*b)%c);
+// console.log(((a%c)*(b%c))%c);
+
+// //13번
+// const fs = require('fs');
+// const inputData = fs.readFileSync('/dev/stdin').toString().split(' ').map(value => +value);
+// const [a,b] = inputData;
+
+
+// //입출력이 되는 부분을 제외하고 a,b를 입력받은 이후의 solution 해결에 집중하자.
+// // B를 문자열로 바꾼뒤, 이를 배열화 한 다음 각 자릿수에 a를 곱해주면 된다.
+// const B = String(b);
+// const Bone = +B[2];
+// const Bten = +B[1];
+// const Bhund = +B[0];
+
+// console.log(a*Bone);
+// console.log(a*Bten);
+// console.log(a*Bhund);
+// console.log(a*b);
